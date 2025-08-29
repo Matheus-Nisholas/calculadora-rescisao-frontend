eb6f43a5c281ea021eee77ddb49c64cca646d153
Matheus Nísholas <nisholas.workspace@outlook.com>
2025-08-29 19:36:48 -0300

    Update calculadora-module.ts

--- src/app/calculadora/calculadora-module.ts
+++ src/app/calculadora/calculadora-module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CalculadoraComponent } from './calculadora.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CalculadoraComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  exports: [
+    CalculadoraComponent
   ]
 })
 export class CalculadoraModule { }
