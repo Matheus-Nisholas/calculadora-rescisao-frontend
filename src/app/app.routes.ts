c4a7818cdf98fdf252ea8b595e7fddbe1f636277
Matheus Nísholas <nisholas.workspace@outlook.com>
2025-08-30 12:03:38 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// ALTERADO: Importamos o nosso novo guarda de rota.
+import { authGuard } from './auth/auth.guard';
+
+import { LoginComponent } from './auth/login/login.component';
+import { LayoutComponent } from './layout/layout.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+
+  {
+    path: 'app',
+    component: LayoutComponent,
+    // ALTERADO: Adicionamos a propriedade `canActivate` e passamos nosso guarda.
+    // Agora, para acessar a rota '/app' ou qualquer uma de suas filhas,
+    // o `authGuard` será executado primeiro.
+    canActivate: [authGuard], 
+    children: [
+      { path: 'calculadora', component: CalculadoraComponent },
+      { path: '', redirectTo: 'calculadora', pathMatch: 'full' }
+    ]
+  },
+
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' }
+];
