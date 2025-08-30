f7d2a7a3465c51dafe6aec8820d4a634457994a8
Matheus Nísholas <nisholas.workspace@outlook.com>
2025-08-30 12:03:43 -0300

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true, // Adicionado para ser standalone
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  // O título não é mais usado no template, mas pode ser mantido.
   protected readonly title = signal('calculadora-rescisao-frontend');
 }
