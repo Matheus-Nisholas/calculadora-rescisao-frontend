1eb810c2006d7d377d1acef77c1dfd55c2316628
Matheus Nísholas <nisholas.workspace@outlook.com>
2025-08-31 18:52:41 -0300

    Update layout.component.ts

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,25 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router'; // Adicionado RouterLink e RouterLinkActive
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
-
 import { AuthService, UserProfile } from '../auth/auth.service';
-
-// NOVO: Importações dos componentes do Angular Material
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon'; // Import para o avatar
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  // ALTERADO: Adicionamos os módulos do Material e do Router ao array de imports
   imports: [
     CommonModule,
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
-    MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule // Adicionado aqui
   ],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
